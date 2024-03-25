fa19bb7f425b94742fa4f69cecc5596a2efd4610
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2024-03-25 10:09:17 +0100

    single film

--- src/pages/films/edit/[titre].vue
+++ src/pages/films/edit/[titre].vue
@@ -1,3 +1,36 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { supabase } from '@/supabase';
+import type { Database } from '@/supabase-types';
+// @ts-ignore
+import { useRoute } from 'vue-router/auto';
+const route = useRoute("/films/edit/[titre]");
+
+
+const film = ref<Database['public']['Tables']['Film']['Row']>();
+
+if (route.params.titre) {
+    let { data, error } = await supabase
+    .from("Film")
+    .select("*")
+    .eq("titre", route.params.titre);
+    if (error) console.log("n'a pas pu charger le table Film :", error);
+    else film.value = (data as any[])[0];
+}
+
+
+</script>
+
+
 <template>
-    oui
+    <div>
+        <p>Titre : {{ film?.titre }}</p>
+        <p>Année de sortie : {{ film?.annee }}</p>
+        <p>Durée : {{ film?.duree_min }}min</p>
+        <p>Réalisateur : {{ film?.realisateur }}</p>
+        <p>Note des spectateurs : {{ film?.note }}</p>
+        <!-- <p>{{ film?.site }}</p> -->
+        <p>Synopsis du film : {{ film?.synopsis }}</p>
+        <iframe width="560" height="315" :src="film?.bande" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
 </template>
